c7076c7cf1b46cfa5241865759d91b9a2f9ea575
Gonzalo Zoloaga <gonzalozoloaga44@gmail.com>
2024-11-10 16:46:12 -0300

    Agrego requirements y modifico ruta del service

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
-from service.rag import get_response
+from src.service.rag import get_response
 
 
 class ChatRequest(BaseModel):
